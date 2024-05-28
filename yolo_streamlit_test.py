4e5a456941d4b5e3e21ebb28b32f68c65172d5fb
JaydenCJH <chlwogud2304@gmail.com>
2024-05-29 01:32:59 +0900

    update temp files. managed to run two_stage_streamlit and use the two stage detector

--- yolo_streamlit_test.py
+++ yolo_streamlit_test.py
@@ -1,10 +1,7 @@
 import streamlit as st
 import numpy as np
-import streamlit as st
 import cv2
-# # from tools import tools, handle_function_call
-from PIL import Image, ImageFont, ImageDraw
-from transformers import pipeline
+from PIL import Image
 import tensorflow as tf
 import torch
 from ultralytics import YOLO
@@ -32,50 +29,45 @@ def set_generate(state=True):
 st.caption("🚀 A streamlit emotion detector by custom model")
 
 
-def img_with_text_results(img, st_frame):
+def img_with_text_results(img):
 
     
     model_path = r"C:\Users\User\Desktop\Code\Github\Final_project\WASSUP_EST_FINAL_Team4\model.pt"
     model = YOLO(model_path)
     # img = cv2.resize(img, (720, int(720 * (9 / 16))))
     
-    res = model.track(img, conf = 0.5, persist = True)
-
-    # res = model.predict(img, 0.5)
+    if torch.cuda.is_available():
+      res = model.track(img, conf = 0.5, persist = True, device = 'cuda' )
+    else:
+      res = model.track(img, conf = 0.5, persist = True)
+    
     res_plotted = res[0].plot()
-    # st_frame.image(res_plotted,
-    #               caption='Detected Video',
-    #               channels="BGR",
-    #               use_column_width=True
-    #               )
+    # id2label = {
+    # '0' : 'Anger',
+    # '1' : 'Happy',
+    # '2' : 'Surprised',
+    # '3' : 'Sadness'
+    # }
+    # yolo 에서 가져온 값들 따로 처리해보기
+    # start_point , end_point = np.array_split(res[0].boxes.xyxy.cpu().numpy().tolist()[0],2)
+    # score = str(round(res[0].boxes.conf.cpu().numpy().tolist()[0]*100,2))+ '%'
+    # label = id2label[str(int(res[0].boxes.cls.cpu().numpy().tolist()[0]))]
+    # results_str = label + ':'+ score
+
+    #cv2 로 박스랑 글자 생성
+    # font = cv2.FONT_HERSHEY_SIMPLEX
+    # blue  = (255, 0, 0)
+    # red = (0, 0, 255)
+
+    # processed_img  = cv2.rectangle(img,(int(start_point[0]), int(start_point[1])), (int(end_point[0]), int(end_point[1])), blue, 3)
+    # processed_img = cv2.putText(processed_img, results_str, (int(start_point[0]), int(start_point[1])) , font, 2, red, 3, cv2.LINE_AA)
+    # processed_img = cv2.cvtColor(processed_img, cv2.COLOR_BGR2RGB)
     
     return res_plotted
+    # return res_plotted, label
 
 
 
-
-
-
-    # results = model.predict(img, save=False, imgsz=320, conf=0.5)
-    
-    # for result in results:
-    #    boxes = result.boxes
-    #    keypoints = results.keypoints
-    #    probs = result.probs
-    
-    # 폰트 색상 지정
-    # blue = (255, 0, 0)
-
-
-    # # 폰트 지정
-    # font = cv2.FONT_HERSHEY_PLAIN
-    # # 이미지에 글자 합성하기
-    # img = cv2.putText(img, results, (30, 40), font, 2, blue, 1, cv2.LINE_AA)
-    # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-
-    # return img
-    # return results
 
 
 run = st.checkbox('Run')
@@ -85,11 +77,12 @@ FRAME_WINDOW = st.image([])
 try:
   while run:
       success, frame = cap.read()
-      st_frame = st.empty()
+      frame = cv2.resize(frame, (640, 480))
 
       # img = img_with_text_results(frame, st_frame)
       if success:
-        img = img_with_text_results(frame, st_frame)
+        img = img_with_text_results(frame)
+        # img, label = img_with_text_results(frame)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         # img = img_with_text_results(frame)
         FRAME_WINDOW.image(img)
@@ -97,8 +90,6 @@ try:
       else:
         cap.release()
         break
-
-
 
   else:
       st.write('Stopped')

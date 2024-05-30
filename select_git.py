3bcdf2fc8c5abb6e67236aa20a5866f9f575d7a5
Woogie0123 <160801709+Woogie0123@users.noreply.github.com>
2024-05-30 15:21:21 +0900

    Update select_git.py

--- select_git.py
+++ select_git.py
@@ -2,7 +2,7 @@ import streamlit as st
 import atm_git
 import kiosk_git
 
-def main():
+def run_select():
     # '장소 선택' 사이드바
     st.sidebar.title('장소 선택')
     selected_location = st.sidebar.radio('현재 위치', ['ATM', '키오스크'])
@@ -14,4 +14,4 @@ def main():
         kiosk_git.main()
 
 if __name__ == "__main__":
-    main()
+    run_select()
